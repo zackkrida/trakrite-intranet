1029e820591bdb4ba6e81c6a04805f9ac91ef999
zackkrida <zackkrida@protonmail.com>
2019-12-16 23:54:27 -0500

    👌 IMPROVE: Switch to preact

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -2,6 +2,17 @@
 const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
+  webpack: function(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: 'preact/compat',
+      react$: 'preact/compat',
+      'react-dom': 'preact/compat',
+      'react-dom$': 'preact/compat',
+    }
+
+    return config
+  },
   experimental: {
     modern: true,
   },
