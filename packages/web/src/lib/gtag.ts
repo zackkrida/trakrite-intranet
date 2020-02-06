5e00357b3ab3866d8648313af0dcb8411c0058b6
zackkrida <zackkrida@protonmail.com>
2020-02-06 07:51:59 -1000

    add google analytics

---
+++ packages/web/src/lib/gtag.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/camelcase */
+export const GA_TRACKING_ID = 'UA-109809072-1'
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = url => {
+  ;(window as any).gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  })
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  ;(window as any).gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
+}
